8a9831d1e224993c9dd8d2b1c281f153680381ef
DeconvFFT <mehta.saumya29@gmail.com>
2022-12-30 16:45:47 -0500

    created session model and service

---
+++ src/models/session.model.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'; 
+import config from 'config';
+import { UserDocument } from './user.model';
+// ts definition for user schema
+// can also do this using typegoose
+export interface SchemaDocument extends mongoose.Document {
+    user: UserDocument['_id'];
+    valid: boolean;
+    password: string;
+    userAgent: string;
+    createdAt: Date;
+    updatedAt: Date;
+
+};
+const sessionSchema = new mongoose.Schema(
+    {
+        user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        valid: {type: Boolean, default: true},,
+        userAgent: {type:String},
+    },
+    {
+    timestamps: true,
+    }
+);
+
+const sessionModel = mongoose.model("Session", sessionSchema);
+
+export default sessionModel;
